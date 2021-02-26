4ae33787adca0af90352a9f31637ef71db5e1476
Mark Dietzer <git@doridian.net>
2021-02-25 23:23:36 -0800

    Fix this

--- config.py
+++ config.py
@@ -9,21 +9,37 @@ class Config:
         self.server = ServerConfig()
         self.addons = []
 
+    def defaults(self):
+        self.server.defaults()
+        for addon in self.addons:
+            addon.defaults()
+
 class AddonConfig:
     def __init__(self):
         self.name = None
         self.repo = None
+        self.private = None
+        self.trusted = None
+
+    def defaults(self):
         self.private = False
         self.trusted = False
 
 class ServerConfig:
     def __init__(self):
+        self.tickrate = None
+        self.maxplayers = None
+        self.map = None
+        self.ip = None
+        self.workshop_clients = None
+        self.workshop_server = None
+        self.gamemode = None
+
+    def defaults(self):
         self.tickrate = 60
         self.maxplayers = 32
         self.map = "gm_flatgrass"
         self.ip = "0.0.0.0"
-        self.workshop_clients = None
-        self.workshop_server = None
         self.gamemode = "sandbox"
 
 def dict_to_obj(dict, o):
@@ -37,7 +53,9 @@ def obj_to_config(dict):
         cfg.inherit = dict["inherit"]
     if "addons" in dict:
         for addon in dict["addons"]:
-            cfg.addons.append(dict_to_obj(addon, AddonConfig()))
+            ac = AddonConfig()
+            ac.defaults()
+            cfg.addons.append(dict_to_obj(addon, ac))
     if "server" in dict:
         cfg.server = dict_to_obj(dict["server"], ServerConfig())
     return cfg
@@ -49,7 +67,7 @@ def merge_object(o1, o2):
     for k in d:
         v = d[k]
         if v != None:
-            setattr(o1, k, d[k])
+            setattr(o1, k, v)
 
 def _load(config):
     fh = open(path.join(CONFIG_DIR, "%s.yml" % config))
@@ -66,6 +84,7 @@ def load(config):
         nextCfg = cfg.inherit
 
     cfg = Config()
+    cfg.defaults()
     while len(cfgStack) > 0:
         nextCfg = cfgStack.pop()
         merge_object(cfg.server, nextCfg.server)
@@ -74,4 +93,7 @@ def load(config):
             merge_object(addonCfg, addon)
             cfg.addons.append(addonCfg)
     cfg.inherit = None
+
+    print(vars(cfg.server))
+
     return cfg
