9117472968848b7ff3851820a35335f3998b4bb7
Mark Dietzer <git@doridian.net>
2021-02-25 22:50:43 -0800

    Add packages needed for srcds

---
+++ misc/apt-install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+export DEBIAN_FRONTEND=noninteractive
+
+echo steam steamcmd/question select "I AGREE" | debconf-set-selections
+echo steam steamcmd/license note '' | debconf-set-selections
+
+dpkg --add-architecture i386
+apt update
+apt -y dist-upgrade
+apt -y install openssl git steamcmd tmux sudo curl htop haveged libsdl2-2.0-0
