a765b45ab9bba8928abf721a7d9ba6cdde29beb4
Mark Dietzer <git@doridian.net>
2021-03-24 18:19:44 -0700

    this

--- misc/apt-install.sh
+++ misc/apt-install.sh
@@ -11,3 +11,5 @@ apt -y dist-upgrade
 apt -y install openssl git steamcmd tmux sudo curl htop haveged libsdl2-2.0-0 python3 python3-yaml python3-requests python3-pip
 
 pip3 install python_a2s
+
+sudo -u server1 mkdir -p /home/server1/s/garrysmod/lua/autorun/server /home/server1/s/garrysmod/cfg /home/server1/s/garrysmod/sa_config
