6fc568d3822dba91645e80ea3a21748a5d738d26
Doridian <git@doridian.net>
2025-04-21 17:11:11 -0700

    Just in case, log it

--- server.py
+++ server.py
@@ -276,7 +276,9 @@ quit
         if not self.proc:
             return
         print("[StarLord] Running: %s" % cmd, flush=True)
-        _ = write(cast(int, self.ptyMaster), b"%s\n" % cmd.encode())
+        data = b"%s\n" % cmd.encode()
+        n = write(cast(int, self.ptyMaster), data)
+        print(f"[StarLord] Wrote {n} out of {len(data)} bytes to pty", flush=True)
 
     def ping(self):
         addr = (self.ip, self.port)
